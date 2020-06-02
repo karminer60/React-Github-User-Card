e8d14e7d59602771e5bcf89f08f71a4a1547202c
karina-rodriguez <karminer60@gmail.com>
2020-06-02 14:28:56 -0700

    added simple code to App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+class App extends React.Component {
+
+  
+    constructor() {
+      super();
+      // initialize state object
+      this.state = {
+        thingsToDo: thingsToDo,
+        anotherStateProperty: "hello web31!",
+        item: "",
+      };
+    }
+
+}
